8623536474f73d8b2ebac76fbec1a2f6916e5cc1
Lucas Boucher <contact@lucasboucher.fr>
2024-02-19 16:18:59 +0100

    ADD: title with crucial font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <p className="text-2xl font-bold">Test de Tailwind</p>
+    <main class="bg-black text-white min-h-screen px-4 pt-8">
+      <h1 className="font-crucial text-2xl">Mon alcool tracker</h1>
+    </main>
   );
 }
 
