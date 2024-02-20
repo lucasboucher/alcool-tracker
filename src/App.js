8aaf1d58a8cf7d57a0a01df8e22db165b16b32ea
Lucas Boucher <contact@lucasboucher.fr>
2024-02-20 14:15:07 +0100

    ADD: consts files

--- src/App.js
+++ src/App.js
@@ -6,35 +6,8 @@ import Result from './components/Result';
 import { GlassEmpty as GlassEmptyIcon } from 'iconoir-react';
 import { PlusCircle as PlusCircleIcon } from 'iconoir-react';
 
-const consumption = [
-  {
-    time: '23:30',
-    centilitersVolume: 50,
-    alcoholContent: 10,
-  },
-  {
-    time: '01:40',
-    centilitersVolume: 40,
-    alcoholContent: 10,
-  },
-  {
-    time: '02:20',
-    centilitersVolume: 30,
-    alcoholContent: 10,
-  },
-  {
-    time: '02:50',
-    centilitersVolume: 45,
-    alcoholContent: 10,
-  },
-  {
-    time: '04:10',
-    centilitersVolume: 10,
-    alcoholContent: 70,
-  },
-];
-
-const bloodAlcoholLevel = 0.45;
+import { bloodAlcoholLevel } from './utils/consts';
+import { consumption } from './utils/consts';
 
 function App() {
   return (
---
+++ src/utils/consts.js
@@ -0,0 +1,29 @@
+export const consumption = [
+  {
+    time: '23:30',
+    centilitersVolume: 50,
+    alcoholContent: 10,
+  },
+  {
+    time: '01:40',
+    centilitersVolume: 40,
+    alcoholContent: 10,
+  },
+  {
+    time: '02:20',
+    centilitersVolume: 30,
+    alcoholContent: 10,
+  },
+  {
+    time: '02:50',
+    centilitersVolume: 45,
+    alcoholContent: 10,
+  },
+  {
+    time: '04:10',
+    centilitersVolume: 10,
+    alcoholContent: 70,
+  },
+];
+
+export const bloodAlcoholLevel = 0.45;
